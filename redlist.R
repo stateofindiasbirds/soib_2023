c37cc14ffbba748e5883ff3d1ef05e1c9322ff7c
Praveen J <paintedstork@gmail.com>
2023-06-02 09:24:09 +0530

    Changes to redlist code to include both linear and exp models and compare
    Changes to redlist code to include both linear and exp models and compare

--- redlist.R
+++ redlist.R
@@ -6,21 +6,26 @@ MaxYear <- 2022
 MinYear <- 2015
 Years <- c(MinYear:MaxYear)
 TrendYears <- MaxYear - MinYear + 1
+threegenperiod <- 2 * (TrendYears - 1) + 100
 
 # Read latest BLI 3gen values
-threegen <- read.csv("3genbli.csv")
-soib <- read.csv("SoIB_main.csv")
+threegen <- read.csv("3genbli.csv") # No in repository. Contact paintedstork@gmail.com
+soib <- read.csv("SoIB_main.csv")   # Available in rep. after SoIB2 runs
 
 # Remove unwanted fields from freq
 soib <- soib %>% select ("India.Checklist.Common.Name",
                          "India.Checklist.Scientific.Name",
                          "BLI.Scientific.Name",
                          "Current.Analysis",
+                         "IUCN.Category",
                          "Selected.SOIB",
                          "totalrange25km",
                          "proprange25km.current",
                          "mean5km",
                          "ci5km",
+                         "longtermrci",
+                         "longtermmean",
+                         "longtermlci",
                          "currentsloperci",
                          "currentslopemean",
                          "currentslopelci",
@@ -36,6 +41,9 @@ soib <- soib %>% select ("India.Checklist.Common.Name",
                          "proj2028.lci",
                          "proj2028.mean",
                          "proj2028.rci",
+                         "proj2029.lci",
+                         "proj2029.mean",
+                         "proj2029.rci",
                          "SOIBv2.Current.Status",
                          "SOIBv2.Priority.Status")
                          
@@ -44,17 +52,14 @@ soib <- soib %>% select ("India.Checklist.Common.Name",
 #  Find of which species is likely to meet some relevant thresholds
 ###################################################################
 
-Years <- c(MinYear:MaxYear)
-TrendYears <- MaxYear - MinYear + 1
-threegenperiod <- 2 * (TrendYears - 1) -1 
 
 # Make a list of species for which soib data is available and declining
 species <- soib %>% 
               filter ( Current.Analysis == "X", #Current trends available
                        currentsloperci < 0, # Declining rci, which is used as a bound in redlisting
-                       mean5km > 10, # Atleast 40% of a grid covered on an average
-                       2 * 1.96 * abs(currentslopemean) > abs(currentsloperci-currentslopelci), # Mean > 2 * SE
-                       (SOIBv2.Current.Status == "Stable") | (SOIBv2.Current.Status == "Declining") | (SOIBv2.Current.Status == "Rapid Decline")) %>%            
+                       mean5km > 8, # Atleast 1/3rd of a grid covered on an average
+                       1.96 * abs(currentslopemean) > abs(currentsloperci-currentslopelci), 
+                       (SOIBv2.Current.Status == "Stable") | (SOIBv2.Current.Status == "Decline") | (SOIBv2.Current.Status == "Rapid Decline")) %>%            
                        select('India.Checklist.Common.Name', 'BLI.Scientific.Name') %>%
                        as.data.frame()
 
@@ -70,18 +75,25 @@ soib <- inner_join (soib, species,by = c("BLI.Scientific.Name" = "BLI.Scientific
 
 soib    <- soib[order(soib$currentsloperci),]
 redlist <- soib %>%
-            mutate( mean_decline =  round (100 * (1 - (1 + currentsloperci/100) ^ GEN),0),
-                    redlist_category1 = case_when (
-                      mean_decline > 80 ~ "Critically Endangered",
-                      mean_decline > 50 ~ "Endangered",
-                      mean_decline > 30 ~ "Vulnerable",
-                      mean_decline > 20 ~ "Near-threatened"
-                    ),            
+            mutate( GEN = ifelse (GEN < 10, 10, GEN),
+                    projected_mean_decline =  round (100 * (1 - (1 + currentsloperci/100) ^ GEN),0),
+                    redlist_category = case_when (
+                      projected_mean_decline > 80 ~ "Critically Endangered",
+                      projected_mean_decline > 50 ~ "Endangered",
+                      projected_mean_decline > 30 ~ "Vulnerable",
+                      projected_mean_decline > 20 ~ "Near-threatened",
+                      projected_mean_decline > 0 ~ "Least Concern"
+                    ),    
+                    projected_mean_decline = paste0(projected_mean_decline,"%"),
+                    longtermmean = -(100 - longtermmean),
+                    longtermlci  = -(100 - longtermlci),
+                    longtermrci  = -(100 - longtermrci),
+                    longtermmean = ifelse(longtermmean == "NA","NA",paste0(round(longtermmean,2),"% (",round(longtermrci,2),", ",round(longtermlci,2),")")),
                     proprange25km.current = round(100 * proprange25km.current,2),
                     currentslopemean = paste0(round(currentslopemean,2),"% (",round(currentsloperci,2),", ",round(currentslopelci,2),")"),
                     mean5km = paste0 ( round (100*mean5km/25, 2),"% (",round(100*(mean5km-ci5km)/25,2),", ",round(100*(mean5km+ci5km)/25,2) ,")"),
                     ci5km = round (ci5km, 2),
-                    redlist_decline = case_when(
+                    projected_decline = case_when(
                     GEN == 5  ~ 100 - `proj2025.rci`,
                     GEN == 6  ~ 100 - `proj2025.rci`,
                     GEN == 7  ~ 100 - `proj2025.rci`,
@@ -90,36 +102,52 @@ redlist <- soib %>%
                     GEN == 10 ~ 100 - `proj2025.rci`,
                     GEN == 11 ~ 100 - `proj2026.rci`,
                     GEN == 12 ~ 100 - `proj2027.rci`,
-                    GEN == 13 ~ 100 - `proj2028.rci`
-                  ), 
-                  redlist_decline = floor(redlist_decline),
-                  redlist_category2 = case_when (
-                    redlist_decline > 80 ~ "Critically Endangered",
-                    redlist_decline > 50 ~ "Endangered",
-                    redlist_decline > 30 ~ "Vulnerable",
-                    redlist_decline > 20 ~ "Near-threatened"
-                  )) %>% 
-        #              filter (redlist_decline > 20) %>%
+                    GEN == 13 ~ 100 - `proj2028.rci`,
+                    GEN == 14 ~ 100 - `proj2029.rci`
+                    ), 
+                    projected_decline = floor(projected_decline),
+                    redlist_category1_exp = case_when (
+                    projected_decline > 80 ~ "Critically Endangered",
+                    projected_decline > 50 ~ "Endangered",
+                    projected_decline > 30 ~ "Vulnerable",
+                    projected_decline > 20 ~ "Near-threatened",
+                    projected_decline > 0  ~ "Least Concern",
+                  ),
+                  redlist_different = ifelse(redlist_category == IUCN.Category,"No","Yes"),
+                  redlist_proj_different = ifelse(redlist_category == redlist_category1_exp,"No","Yes")
+                  ) %>% 
                         select ("India.Checklist.Common.Name.x",
                                 "proprange25km.current",
                                 "mean5km",
+                                "longtermmean",
                                 "currentslopemean",
                                 "SOIBv2.Current.Status",
                                 "SOIBv2.Priority.Status",
-                                "mean_decline",
-                                "redlist_category1",
-                                "redlist_decline",
-                                "redlist_category2")
+                                "GEN",
+                                "projected_mean_decline",
+                                "redlist_category",
+                                "IUCN.Category",
+                                "redlist_different",
+                                "redlist_category1_exp",
+                                "redlist_proj_different",
+                                "projected_decline"
+                                )
 
 colnames(redlist) <- c ("Species",
                      "Range Coverage",
                      "Mean Grid Coverage",
-                     "Annual Decline",
+                     "Long-term Decline",
+                     "Current Annual Decline",
                      "Current Status",
                      "Conservation Priority",
-                     "Mean Decline",
-                     "Mininum Redlist Decline",
-                     "Redlist Category1 Proposed",
-                     "Redlist Category2 Proposed")
+                     "3GEN",
+                     "3GEN Decline",
+                     "Redlist Category Proposed",
+                     "Current Redlist",
+                     "IUCN Different",
+                     "Redlist using Exp projections",
+                     "Redlist projections different",
+                     "Projected Decline"
+                     )
 
 write.csv(redlist, "redlist.csv", row.names = FALSE)
