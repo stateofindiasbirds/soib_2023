82b70b094bdc5661d274e4d93ca9b7e40b13bdc3
birdcountindia <skimmer@birdcount.in>
2023-07-26 08:24:54 +0530

    separate script for create metadata + initialised plot output paths

--- 00_pipeline.R
+++ 00_pipeline.R
@@ -7,65 +7,7 @@ source("00_scripts/00_functions.R")
 
 # PART 0 (paths) ----------------------------------------------------------
 
-# create and save metadata for processing analyses for various masks.
-# (run only when there are changes to paths. else loaded directly in each Step.)
-
-states_list <- c(
-  "Goa", "Manipur", "Odisha", "Dadra and Nagar Haveli", "Andaman and Nicobar Islands",
-  "Bihar", "Chandigarh", "Karnataka", "Lakshadweep", "Rajasthan", "Arunachal Pradesh",
-  "Gujarat", "Haryana", "Mizoram", "Uttar Pradesh", "Uttarakhand", "Himachal Pradesh",
-  "Daman and Diu", "Sikkim", "Delhi", "Tripura", "Punjab", "Madhya Pradesh", "Jharkhand",
-  "Nagaland", "Telangana", "West Bengal", "Chhattisgarh", "Jammu and Kashmir", "Assam",       
-  "Maharashtra", "Meghalaya", "Ladakh", "Kerala", "Tamil Nadu", "Andhra Pradesh", "Puducherry"
-)
-
-analyses_metadata <- data.frame(MASK = c("none", 
-                                         "woodland", "cropland", "ONEland", 
-                                         "PA",
-                                         states_list)) %>% 
-  # whether it is for country, state or habitat masks
-  mutate(MASK.TYPE = case_when(
-    MASK == "none" ~ "country", 
-    MASK %in% c("woodland", "cropland", "ONEland") ~ "habitat",
-    MASK == "PA" ~ "PA", 
-    TRUE ~ "state"
-  )) %>% 
-  mutate(FOLDER = case_when(
-    MASK == "none" ~ "01_analyses_full/", 
-    MASK %in% c("woodland", "cropland", "ONEland", "PA") ~ glue("01_analyses_mask-{MASK}/"),
-    TRUE ~ glue("01_analyses_states/{MASK}/")
-  )) %>% 
-  mutate(FULLSPECLIST.PATH = glue("{FOLDER}fullspecieslist.csv"),
-         LOCS.PATH = glue("{FOLDER}sub_samp_locs.csv"),
-         SPECLISTDATA.PATH = glue("{FOLDER}specieslists.RData"),
-         DATA.PATH = glue("{FOLDER}dataforanalyses.RData"),
-         RAND.GROUP.IDS.PATH = glue("{FOLDER}randomgroupids.RData"),
-         
-         SIMDATA.PATHONLY = glue("{FOLDER}dataforsim/"),
-         TRENDS.PATHONLY = glue("{FOLDER}trends/"),
-         OCCU.PRES.PATHONLY = glue("{FOLDER}occupancy-presence/"),
-         OCCU.MOD.PATHONLY = glue("{FOLDER}occupancy-model/"),
-         RESULTS = glue("{FOLDER}results/"),
-         
-         OCCU.OUTPATH = glue("{RESULTS}occupancy/"),
-
-         TRENDS.OUTPATH = glue("{RESULTS}trends.csv"),
-         CURSENS.PATH = glue("{RESULTS}current_sensitivity.csv"),
-         SOIBMAIN.WOCATS.PATH = glue("{RESULTS}SoIB_main_wocats.csv"),
-         SOIBMAIN.PATH = glue("{RESULTS}SoIB_main.csv"),
-         SUMMARY.PATH = glue("{RESULTS}SoIB_summaries.xlsx"))
-
-# ensuring folders are created if they don't already exist
-walk2(analyses_metadata$FOLDER, analyses_metadata$RESULTS, ~ {
-  
-  if (!dir.exists(.x)) {dir.create(.x, recursive = TRUE)}
-  
-  if (!dir.exists(.y)) {dir.create(.y, recursive = TRUE)}
-  
-})
-
-# for later reference
-save(analyses_metadata, file = "00_data/analyses_metadata.RData")
+source("00_scripts/01_create_metadata.R")
 
 
 # PART 1 ------------------------------------------------------------------
