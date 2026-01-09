adf2221eec40e3d9f564fa14625727a7df3bd22c
shasanko <ongoleshasank@gmail.com>
2026-01-09 11:53:14 +0530

    Changes to ensure that only trend csv files (and not other folders) are picked for resolve

--- 00_scripts/resolve_trends_and_occupancy.R
+++ 00_scripts/resolve_trends_and_occupancy.R
@@ -147,7 +147,7 @@ if (run_res_trends == FALSE) {
   
   trends_files <- trends[!file.info(trends)$isdir]
   
-  trends <- trends_files %>%
+  trends <- trends_files %>% 
     # Read each CSV file and combine them into a single data frame
     map_df(read.csv) %>%
     mutate(timegroups = if_else(is.na(timegroups) & timegroupsf == "before 2000", 1990, timegroups))
