76a4eec4c9aa20d42bca6a6b2da6f62ad17f49cc
Praveen J <paintedstork@gmail.com>
2026-06-01 10:07:35 +0530

    Changes to config file to pick from single datapath (Correct file)
    Changes to config file to pick from single datapath
    Moved all absolute paths to relative, relative to project root directory. This needs library (here). Please install it.

--- 00_scripts/WPE-Scripts/EU-OT_Population_Estimate/config.R
+++ 00_scripts/WPE-Scripts/EU-OT_Population_Estimate/config.R
@@ -2,29 +2,35 @@
 # Data and Mapping files - User-specific configuration
 ##########################################################
 
-# EU and OT google sheet file
+library(here)
+
+# Data directory (outside project root)
+# Change here if you have a different project root
+datapath <- here("..", "data")
+
+# EU and OT Google Sheet
 eut_and_ot <- "https://docs.google.com/spreadsheets/d/1pNW5qFOGug1igvMLBS3-101bVYg_iLvpdMNs8suDQrg"
 
 # eBird data
-ebird <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/Data and Codes - from Linux Server/Data to test EU-OT/ebird_data.csv"
+ebird <- file.path(datapath, "ebird_data.csv")
 
 # AWC 2005 to 2025 data - from Apoorva
-awc <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/AWC Data/AWC 2005-2025/AWC 2005-25_copy.xlsx"
+awc <- file.path(datapath, "AWC 2005-25_copy.xlsx")
 
 # AWC 2026 raw data
-awc26 <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/AWC Data/AWC - PraveenJ/AWC_2026_RawData/project-1051-report-20260203_0420.txt"
+awc26 <- file.path(datapath, "project-1051-report-20260203_0420.txt")
 
 # Taxonomy mapping file
-taxonomy_mapping <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/Data and Codes - from Linux Server/Data to test EU-OT/Taxonomy Mapping Table.csv"
+taxonomy_mapping <- file.path(datapath, "Taxonomy Mapping Table.csv")
 
 # CAF Species List - AWC names
-CAF_species_list <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/Data and Codes - from Linux Server/Data to test EU-OT/CAF_species_list.csv"
+CAF_species_list <- file.path(datapath, "CAF_species_list_BLI_names.csv")
 
 # CAF Species List - eBird names
-CAF_species_list_ebird_names <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/Data/species_list.csv"
+CAF_species_list_ebird_names <- file.path(datapath, "CAF_species_list_ebird_names.csv")
 
-# District level Shapefile for India
-district_shapefile <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/Data/maps_sf.RDATA"
+# District level shapefile for India
+district_shapefile <- file.path(datapath, "maps_sf.RDATA")
 
 # eBird State and District Codes
-all_region_codes <- "D:/NCF - Bird Mon/SoIB - Wetland Population Estimate/Data/Region Codes.csv"
+all_region_codes <- file.path(datapath, "Region Codes.csv")
